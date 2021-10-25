d05ab7aaa56c841f1063c3f9e2f599bc3f24cf32
Stanislav <staniskudriavtsev@yandex.ru>
2021-10-25 23:30:45 +0300

    gnl: completed

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stanislav <stanislav@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 23:25:48 by stanislav         #+#    #+#             */
+/*   Updated: 2021/10/25 23:25:49 by stanislav        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdbool.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4095
+# endif
+
+# if (BUFFER_SIZE) <= 0
+#  error Invalid buffer of (BUFFER_SIZE) size
+# endif
+
+# define CACHE_SIZE 255
+
+// bp/cp - current position in the buffer/cache arrays.
+// nbs - number of bytes read from fd into the buffer.
+typedef struct s_fdb
+{
+	int				fd;
+	char			buf[BUFFER_SIZE + 1];
+	size_t			bp;
+	char			cache[CACHE_SIZE + 1];
+	size_t			cp;
+	ssize_t			nbs;
+	struct s_fdb	*next;
+}	t_fdb;
+
+char	*get_next_line(int fd);
+
+char	*gnl_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memset(void *s, int c, size_t n);
+
+#endif
